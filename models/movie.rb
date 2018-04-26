48a8d1a73f6ea6212e8b12ad4ddd9b6533e68620
davina <davina.kuhnel@gmail.com>
2018-04-26 15:29:33 +0100

    set up star and movie classes

---
+++ models/movie.rb
@@ -0,0 +1,15 @@
+require_relative("../db/sql_runner")
+
+class Movie
+
+  attr_reader :id, :title, :genre, :rating
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @title = options["title"]
+    @genre = options["genre"]
+    @rating = options["rating"].to_i
+  end
+
+
+
+end
